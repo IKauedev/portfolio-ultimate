13795a1fb7345a00c84677156a251cf360064c67
kaue matos <kaue178hotimail@gmail.com>
2023-12-25 10:03:58 -0300

    fix:correção dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { ThemeProvider } from "styled-components";
 import './App.css';
 import LayoutRouter from "./router/router.jsx";
-import { darkTheme, lightTheme } from './utils/Themes.js';
+import { darkTheme } from './utils/Themes.js';
 
 export default function App() {
 
   return (
-    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+    <ThemeProvider theme={darkTheme}>
       <LayoutRouter />
     </ThemeProvider>
   );
