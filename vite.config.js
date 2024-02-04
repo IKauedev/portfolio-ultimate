fb18e6f17b9d93d5fb2563827a27882393dbc117
kaue matos <kaue178hotimail@gmail.com>
2024-02-04 17:13:48 -0300

    atualização

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import react from '@vitejs/plugin-react';
+import ViteCompression from 'vite-plugin-compression';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default {
+  plugins: [
+    react(),
+    ViteCompression({
+      verbose: true,
+      disable: false,
+      threshold: 10240,
+      algorithm: 'gzip',
+      ext: '.gz',
+    }),
+  ],
+  build: {
+    outDir: 'build',
+    assetsDir: '',
+    minify: 'terser',
+    chunkSizeWarningLimit: 2000,
+    brotliSize: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+};
