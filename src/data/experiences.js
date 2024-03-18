30bd45952726b6d8f775a16d00214f38afe3c752
Kaue matos <kaue178hotimail@gmail.com>
2024-03-18 19:18:55 -0300

    update: professional

--- src/data/experiences.js
+++ src/data/experiences.js
@@ -1,45 +1,44 @@
 
 export const experiences = [
-    {
-      id: 1,
-      img: "https://cdn.icon-icons.com/icons2/2699/PNG/512/freelancer_logo_icon_171122.png",
-      role: "Freelancer",
-      company: "",
-      date: "Fev 2023 - Atual",
-      desc: "Como desenvolvedor back-end, juntamente com outros desenvolvedores foi acordado uma criação de um website para uma microempresa da região",
-      skills: [
-        "Docker",
-        "AWS",
-        "React.js",
-        "JavaScript",
-        "Java",
-        "SpringBoot"
-      ],
-      doc: "",
-    },
-    {
-      id: 2,
-      img: "https://i.postimg.cc/qR4pYHW1/logo-live-ecommerce.jpg",
-      role: "Estagiario de Desenvolvimento Back-end",
-      company: "Live Ecommerce",
-      date: "Jan 2023 - Dez - 2023",
-      desc: "Durante minha experiência, atuei na implementação de novas funcionalidades e na resolução de problemas por meio de desenvolvimento back-end em PHP, em conjunto com tecnologias como JavaScript com jQuery, HTML e CSS. Meu trabalho envolveu aprimorar o desempenho e a usabilidade de sistemas, contribuindo para a melhoria contínua do software.",
-      skills: [
-        "PHP",
-        "HTML",
-        "CSS",
-        "JavaScript",
-      ]
-    },
-    {
-      id: 3,
-      img: "https://i.postimg.cc/1tKmgcQx/decco.png",
-      role: "Auxiliar de Logistica",
-      company: "Delute Comercial",
-      date: "Out 2021 - Atual",
-      desc: "Como uma pessoa que trabalhou como auxiliar de logística em seu primeiro emprego, adquiri valiosas experiências e desenvolvi habilidades essenciais para o setor. Durante esse período, desempenhei um papel fundamental no suporte às atividades logísticas diárias da empresa Minhas principais responsabilidades incluíam o recebimento, armazenamento e expedição de mercadorias. Trabalhei em estreita colaboração com a equipe para garantir que os produtos fossem corretamente identificados, embalados e registrados em sistemas de gerenciamento de estoque. Além disso, também auxiliei na organização do armazém, realizando inventários periódicos e mantendo a área de trabalho limpa e segura. Durante o período em que trabalhei como auxiliar de logística, desenvolvi habilidades de comunicação eficaz e trabalho em equipe. Foi essencial manter uma comunicação clara com os membros da equipe e com outros departamentos, a fim de garantir uma coordenação adequada das atividades logísticas.",
-      skills: [
-        "Marketing",
-      ]
-    },
-  ];
+  {
+    id: 1,
+    img: "https://i.postimg.cc/QM7khYD6/icon.jpg",
+    role: "Nova Software",
+    company: "",
+    date: "Fev 2023 - Atual",
+    desc: "Como CEO da Nova Software e também desenvolvedor, estou em uma posição única que permite entender tanto os aspectos estratégicos e de negócios quanto os aspectos técnicos do desenvolvimento do software está sendo um desafio muito gratificante, por me dar a capacidade de desenvolver um produto onde irei fornencer para diversos usuarios e cumprir com proposito real, Ademais a Nova software tem objetivo de fornecer serviços para um usuario que deseja começar no ramo de vendas online prevendo até mesmo situações do dia a dia.",
+    skills: [
+      "Docker",
+      "AWS",
+      "React.js",
+      "JavaScript",
+      "Java",
+      "SpringBoot"
+    ],
+  },
+  {
+    id: 2,
+    img: "https://i.postimg.cc/qR4pYHW1/logo-live-ecommerce.jpg",
+    role: "Estagiario de Desenvolvimento Back-end",
+    company: "Live Ecommerce",
+    date: "Jan 2023 - Dez - 2023",
+    desc: "Durante minha experiência, atuei na implementação de novas funcionalidades e na resolução de problemas por meio de desenvolvimento back-end em PHP, em conjunto com tecnologias como JavaScript com jQuery, HTML e CSS. Meu trabalho envolveu aprimorar o desempenho e a usabilidade de sistemas, contribuindo para a melhoria contínua do software.",
+    skills: [
+      "PHP",
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ]
+  },
+  {
+    id: 3,
+    img: "https://i.postimg.cc/1tKmgcQx/decco.png",
+    role: "Auxiliar de Logistica",
+    company: "Delute Comercial",
+    date: "Out 2021 - Atual",
+    desc: "Como uma pessoa que trabalhou como auxiliar de logística em seu primeiro emprego, adquiri valiosas experiências e desenvolvi habilidades essenciais para o setor. Durante esse período, desempenhei um papel fundamental no suporte às atividades logísticas diárias da empresa Minhas principais responsabilidades incluíam o recebimento, armazenamento e expedição de mercadorias. Trabalhei em estreita colaboração com a equipe para garantir que os produtos fossem corretamente identificados, embalados e registrados em sistemas de gerenciamento de estoque. Além disso, também auxiliei na organização do armazém, realizando inventários periódicos e mantendo a área de trabalho limpa e segura. Durante o período em que trabalhei como auxiliar de logística, desenvolvi habilidades de comunicação eficaz e trabalho em equipe. Foi essencial manter uma comunicação clara com os membros da equipe e com outros departamentos, a fim de garantir uma coordenação adequada das atividades logísticas.",
+    skills: [
+      "Marketing",
+    ]
+  },
+];
