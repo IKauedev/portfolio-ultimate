6b4b02e6261cf5d9f906bf2fb088bad007452815
Kaue matos <kaue178hotimail@gmail.com>
2024-04-04 07:07:27 -0300

    update curriculo

--- src/data/bio.js
+++ src/data/bio.js
@@ -1,14 +1,14 @@
 export const Bio = {
-    name: "Kauê de Matos",
-    roles: [
-      "Desenvolvedor Back-end",
-      "Programador",
-    ],
-    description:
-      "Sou uma pessoa motivada e versátil, sempre disposta a enfrentar novos desafios. Com paixão por aprender, me dedico a entregar resultados de alta qualidade. Com uma atitude positiva e uma mentalidade construtiva, estou pronto para dar uma contribuição significativa e alcançar grandes feitos.",
-    github: "https://github.com/iKaueMatos",
-    resume: "https://drive.google.com/file/d/1NivSpo7kSD7dvcmPJyeyywzQoiR_jxVo/view?usp=sharing",
-    linkedin: "https://www.linkedin.com/in/ikauematos/",
-    insta: "https://www.instagram.com/ikauedev/",
-    dev: "https://i.postimg.cc/xT3x9Dkw/icons8-desenvolvedor-64.png"
-  };
+  name: "Kauê de Matos",
+  roles: [
+    "Desenvolvedor Back-end",
+    "Programador",
+  ],
+  description:
+    "Sou uma pessoa motivada e versátil, sempre disposta a enfrentar novos desafios. Com paixão por aprender, me dedico a entregar resultados de alta qualidade. Com uma atitude positiva e uma mentalidade construtiva, estou pronto para dar uma contribuição significativa e alcançar grandes feitos.",
+  github: "https://github.com/iKaueMatos",
+  resume: "https://docs.google.com/document/d/1Glu9zI1JT-M7t3_PUprespHvd1oHnntFQZAmHQx29Hc/edit?usp=drive_link",
+  linkedin: "https://www.linkedin.com/in/ikauematos/",
+  insta: "https://www.instagram.com/ikauedev/",
+  dev: "https://i.postimg.cc/xT3x9Dkw/icons8-desenvolvedor-64.png"
+};
