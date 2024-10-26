b4874ee3f03ecc79e3a548e94470573f3db000bb
ikauedev <kaue178hotimail@gmail.com>
2024-10-25 22:13:55 -0300

    ikauedev 4.0

--- src/pages/Home/HeroSection/index.jsx
+++ src/pages/Home/HeroSection/index.jsx
@@ -1,6 +1,6 @@
 import { MdDownload } from "react-icons/md";
 import Typewriter from "typewriter-effect";
-import profile from "../../../assets/images/ikauedev3.0.jpg";
+import profile from "../../../assets/images/ikauedev4.0.jpg";
 import { Bio } from "../../../data/bio.js";
 import HeroBgAnimation from "../HeroBgAnimation/index.jsx";
 
